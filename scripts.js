26798b01264d54c3f0564fe2336c79381a042eb5
damullings17 <daniellemullings17@gmail.com>
2020-11-01 05:33:49 -0500

    Add Email checker in JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
 /* Add your JavaScript to this file */
+window.onload = function()
+{
+    btns = document.getElementsByClassName("btn")
+    
+    
+    var message = document.getElementsByClassName("message")
+    message = message[0]
+
+    btns[1].addEventListener("click", function(){
+        var email = document.getElementById("email").value
+        if (email == "")
+        {
+            message.innerHTML = "Please enter a valid email address"
+
+        }
+        else
+        {
+            message.innerHTML = "Thank you! Your email address " + email + " has been added to our mailing list"
+
+        }
+    
+    
+    })
+}
+
